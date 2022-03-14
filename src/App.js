8ee8edccde6297e6c8ddee230a855030195d7019
Eliza Fury <Ejfury@protonmail.com>
2022-03-14 11:08:07 +1000

    Style and align text

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ const api = {
 }
 
 function App() {
+
+    const dateBuilder = (d) => {
+      let months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`
+    }
      return (
        <div className="app warm">
         <main>
@@ -16,6 +28,16 @@ function App() {
               className="search-bar"
               placeholder="Search.."
             />
+            <div className="location-box">
+              <div className="location"> New York City US</div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              15
+            </div>
+            <div className="weather">Sunny</div>
           </div>
         </main>
       </div>
