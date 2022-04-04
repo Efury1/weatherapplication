f8e2609ef1872f2cd5ae5d19537f30c966f13ebd
Eliza Fury <elizafury@Elizas-MacBook-Pro.local>
2022-04-05 01:40:28 +1000

    resize location box

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
             />
           </div>
           {(typeof weather.main != "undefined") ? (
-          <div>
+          <div class="card">
             <div className="location-box">
               <div className="location">{weather.name}, {weather.sys.country}</div>
               <div className="date">{dateBuilder(new Date())}</div>
